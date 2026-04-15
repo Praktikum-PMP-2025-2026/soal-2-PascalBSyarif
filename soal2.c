5052b5b64e230db8eec59ff131fcd28df9bff619
PascalBSyarif <pascalbsyarif@gmail.com>
2026-04-15 08:40:42 +0700

    soal2.c

--- soal2.c
+++ soal2.c
@@ -1,3 +1,13 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 1
+ *   Hari dan Tanggal    : Rabu, 15 April 2026
+ *   Nama (NIM)          : Pascal Baihaqi Syarif (13224027)
+ *   Nama File           : soal2.c
+ *   Deskripsi           : Sebuah program yang menerima T bilangan input n1 n2 ... nT dan melakukan rekursi dengan dua case
+ *                         hingga semua bilangan n1 n2 ... nT mengecil menjadi 1. jumlah langkah iterasi rekursi untuk masing
+ *                         masing bilangan n akan di outputkan di akhir program
+ * 
+ */
 #include <stdio.h>
 
 int main(){
@@ -24,4 +34,6 @@ int main(){
         }
         printf("LANGKAH %d\n", count);
     }
+    return 0;
+    // Tidak ada referensi yang digunakan
 }
